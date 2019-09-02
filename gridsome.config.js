07fb7c84787147f0b99a6e3ab5a1d97c2b6ce57c
Arnaud Lecat <a.lecat@nonozone.com>
2019-09-02 16:37:23 +0200

    wip

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,20 +5,27 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'CleverFields',
-  plugins: [{
-    use: 'gridsome-plugin-tailwindcss',
-    options: {
-      purgeConfig: {},
-      presetEnvConfig: {},
-      shouldPurge: true,
-      shouldImport: true,
-      shouldTimeTravel: true,
-    }
-  }, {
-    use: '@gridsome/plugin-google-analytics',
-    options: {
-      id: 'UA-XXXXXXXXX-X'
-    }
-  }]
+    siteName: 'CleverFields',
+    siteDescription: 'A Reims, CleverFields vous aide à concevoir, analyser et pérénniser vos applications Web et Mobile.',
+    siteUrl: 'https://cleverfields.fr',
+    plugins: [{
+        use: 'gridsome-plugin-tailwindcss',
+        options: {
+            purgeConfig: {},
+            presetEnvConfig: {},
+            shouldPurge: true,
+            shouldImport: true,
+            shouldTimeTravel: true,
+        }
+    }, {
+        use: '@gridsome/plugin-google-analytics',
+        options: {
+            id: 'UA-XXXXXXXXX-X'
+        }
+    }, {
+        use: '@gridsome/plugin-sitemap',
+        options: {
+            cacheTime: 600000, // default
+        }
+    }]
 }
