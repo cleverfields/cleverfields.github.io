8933cf0976fc8509da1bafd6caa591b7a8c8adf0
Arnaud Lecat <a.lecat@nonozone.com>
2020-06-02 16:20:18 +0200

    Last chance before deleting master

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,30 +5,33 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: 'CleverFields',
-    siteDescription: 'A Reims, CleverFields vous aide à concevoir, analyser et pérénniser vos applications Web et Mobile.',
-    siteUrl: 'https://cleverfields.fr',
-    plugins: [{
-            use: 'gridsome-plugin-tailwindcss',
+    siteName: "CleverFields",
+    siteDescription:
+        "A Reims, CleverFields vous aide à concevoir, analyser et pérénniser vos applications Web et Mobile.",
+    siteUrl: "https://cleverfields.github.io",
+    pathPrefix: "/cleverfields.github.io",
+    plugins: [
+        {
+            use: "gridsome-plugin-tailwindcss",
             options: {
                 purgeConfig: {},
                 presetEnvConfig: {},
                 shouldPurge: true,
                 shouldImport: true,
-                shouldTimeTravel: true,
+                shouldTimeTravel: true
             }
         },
         {
-            use: '@gridsome/plugin-google-analytics',
+            use: "@gridsome/plugin-google-analytics",
             options: {
-                id: 'UA-XXXXXXXXX-X'
+                id: "UA-XXXXXXXXX-X"
             }
         },
         {
-            use: '@gridsome/plugin-sitemap',
+            use: "@gridsome/plugin-sitemap",
             options: {
-                cacheTime: 600000, // default
+                cacheTime: 600000 // default
             }
         }
     ]
-}
+};
